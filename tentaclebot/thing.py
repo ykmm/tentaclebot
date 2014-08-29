83bbd2f941c9caf430f5ba521445671496da7639
Yann <yakemema@gmail.com>
2014-08-29 16:33:54 +0200

    Added mimetype detection methods and file saving method to Thing class.

--- tentaclebot/thing.py
+++ tentaclebot/thing.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import logging
+logger = logging.getLogger(__name__)
+import os
 import tempfile
-from docextractor.docextractor import DocExtractor, pycurl
+from docextractor.docextractor import DocExtractor
 from httprep.url import Url
+
+import magic
+
+class MimeType(object):
+
+    def __init__(self, mimetype):
+        self.mimetype = mimetype
+        self.mtype, self.msubtype = self.mimetype.split("/")
 
 class Thing(object):
     """Representes something to be downloaded
@@ -29,6 +39,7 @@ class Thing(object):
 
     def dereference_url(self):
         if not self._content_aquired:
+            logger.debug("Downloading...")
             self._content_aquired = True
             DE = DocExtractor()
             try:
@@ -37,9 +48,23 @@ class Thing(object):
                 _temp_file = tempfile.NamedTemporaryFile(delete=False)
                 _temp_file.write(self._http_data)
                 _temp_file.close()
-                self._temp_file_name = _temp_file.name
+                self.filename = _temp_file.name
             except:
                 raise
+
+    def save_file(self, path):
+        """Moves the temporary in the folder specified by `path`"""
+        try:
+            filename = os.path.basename(self.url.path)
+            if len(filename)==0:
+                filename = os.path.basename(thing.temp_file)
+            os.rename(self.temp_file, path+filename)
+        except:
+            raise
+        else:
+            self.filename = path+filename
+            logger.debug("File saved as %s" % (self.filename))
+            return True
 
     #TODO, add __del__ method to delete temporary file if created
 
@@ -61,4 +86,10 @@ class Thing(object):
     @property
     def temp_file(self):
         self.dereference_url()
-        return self._temp_file_name
+        return self.filename
+
+    @property
+    def mimetype(self):
+        self.dereference_url()
+        return MimeType(magic.from_file(self.temp_file, mime=True))
+
