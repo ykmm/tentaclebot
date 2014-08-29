36c049f328cfd6372391c099b9106b2d5790078b
Yann <yakemema@gmail.com>
2014-08-29 16:50:02 +0200

    Code refactoring, use Thing() save_file method.

--- tentaclebot/tentacles/torrent.py
+++ tentaclebot/tentacles/torrent.py
@@ -4,7 +4,6 @@
 WClassName = 'TorrentTentacle'
 
 import multiprocessing
-import filetype
 import os
 
 import logging
@@ -20,8 +19,7 @@ class TorrentTentacle(multiprocessing.Process):
     def supports(self, thing):
         #Verifica se è un link magnet e facci qualcosa
         if thing.url.scheme in ('http','https'):
-            _ft = filetype.filetype(thing.temp_file)
-            if _ft == 'BitTorrent file':
+            if thing.mimetype.mimetype == 'application/x-bittorrent':
                 return True
             else:
                 return False
@@ -39,19 +37,14 @@ class TorrentTentacle(multiprocessing.Process):
 
             jid, thing = next_task
 
-            #SAVE FILE IN MEDIA DIRECTORY
             if thing.url.scheme in ('http','https'):
-                #ANSWER BACK TO USER
-
-                message = "ALL OK MASTER %s %s" % (thing.url.href(), thing.temp_file)
-
                 logger.info(thing.http_headers)
-                filename = os.path.basename(thing.url.path)
-                if len(filename)==0:
-                    filename = os.path.basename(thing.temp_file)
-                os.rename(thing.temp_file, self.save_dir+filename)
-                logger.debug("saving file %s" % (self.save_dir+filename))
+                if thing.save_file(self.save_dir):
+                    message = "ALL OK MASTER %s downloaded as %s" % (thing.url.href(), thing.filename)
+                else:
+                    message = "I WAS NOT ABLE TO DO STUFF MASTER"
                 self.out_queue.put((jid, message))
+
             if thing.url.scheme in ('magnet'):
                 message = "I DON'T KNOW HOW TO DEAL WITH MAGNET LINKS YET, TEACH ME MASTER, PLEASE!"
                 self.out_queue.put((jid, message))
